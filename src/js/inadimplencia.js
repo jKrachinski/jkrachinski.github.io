563c55e5ec7c3d17ecfd5190fbfcbcb2ae479b0d
jKrachinski <129908892+jKrachinski@users.noreply.github.com>
2024-05-23 22:12:13 -0300

    Add files via upload

--- src/js/inadimplencia.js
+++ src/js/inadimplencia.js
@@ -7,10 +7,18 @@ var CASD = document.getElementById("inadimplencia-CASD")
 var copinha = document.getElementById("inadimplencia-copinha")
 var evtol = document.getElementById("inadimplencia-evtol")
 
-window.addEventListener('load', () => {
+function load_inadimplencia() {
     evtol.innerHTML = "A reforma de " + `<span style="font-weight: bold;">${String(deficit['evtol'])}</span>` + " salas da EVTOL"
     gaga8.innerHTML = "A construção de " + `<span style="font-weight: bold;">${String(deficit['gaga8'])}</span>` + " gaga8"
     copinha.innerHTML = "A construção de " + `<span style="font-weight: bold;">${String(deficit['copinha'])}</span>`  + " copinhas"
     CASD.innerHTML = "A reforma de " + `<span style="font-weight: bold;">${String(deficit['CASD'])}</span>` + " banheiros do CASDVest"
     inadimplenciaGeral.innerHTML = "R$ " + String(deficit['deficit']) + ",00"
-})
+}
+
+if (document.readyState === "complete") {
+    load_inadimplencia()
+    console.log('readySate')
+  } else {
+    window.addEventListener('load', load_inadimplencia());
+    console.log('event here')
+  }
